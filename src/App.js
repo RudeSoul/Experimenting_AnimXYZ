d1b94b139d05b8da82ac318d45fc9fa20aeb1106
rudesoul <prabesh7@gmail.com>
2024-01-16 13:05:41 -0500

    - trying out animXYZ - added header, body and footer with some animation using animXYZ

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "@animxyz/core";
+import Header from "./Header.jsx";
+import { XyzTransition } from "@animxyz/react";
+import "./style.css";
+import Body from "./Body.jsx";
+import Footer from "./Footer.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <XyzTransition appear duration="auto">
+      <div className="page-wrap">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    </XyzTransition>
   );
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div
+      className="page-hero"
+      xyz="fade small stagger ease-out-back duration-30"
+    >
+      <div className="hero-logo xyz-nested"></div>
+      <p className="hero-text xyz-nested">Some text here</p>
+    </div>
+  );
+}
